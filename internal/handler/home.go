21ccabc4b7dfaff26aec9d1683e7ed9c1d700a5d
Saul Pinales <saul021002@gmail.com>
2022-10-19 21:14:38 -0400

    simple template rendering

--- internal/handler/home.go
+++ internal/handler/home.go
@@ -2,12 +2,29 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"text/template"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "Hello World!!")
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	ts, err := template.ParseFiles("./web/templates/landing.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func DashboardHandler(w http.ResponseWriter, r *http.Request) {
