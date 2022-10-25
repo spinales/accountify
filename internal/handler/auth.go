7cc42104873af050615e7c190ed2b52e60a4a37d
spinales <saul021002@gmail.com>
2022-10-25 00:11:46 -0400

    signup, login and recovery password pages

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,16 +1,70 @@
 package handler
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "Login!!")
+	files := []string{
+		"./web/templates/login.page.tmpl",
+		"./web/templates/base.layout.tmpl",
+		"./web/templates/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "Sign Up")
+	files := []string{
+		"./web/templates/signup.page.tmpl",
+		"./web/templates/base.layout.tmpl",
+		"./web/templates/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func RecoveryHandler(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./web/templates/recovery.page.tmpl",
+		"./web/templates/base.layout.tmpl",
+		"./web/templates/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
