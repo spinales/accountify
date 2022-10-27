3df2ab567ac48238ce45f09763f009d88be2de0e
spinales <saul021002@gmail.com>
2022-10-26 23:32:34 -0400

    static files server

--- cmd/accountify/main.go
+++ cmd/accountify/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+	fileserver := http.FileServer(http.Dir("./web/css/"))
 
 	// handlers routes
 	r.HandleFunc("/", handler.HomeHandler)
@@ -17,6 +18,9 @@ func main() {
 	r.HandleFunc("/signup", handler.SignUpHandler)
 	r.HandleFunc("/contact", handler.ContactHandler)
 	r.HandleFunc("/recovery", handler.RecoveryHandler)
+	// r.Handle("/static/", http.StripPrefix("/static", fileserver))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fileserver))
+	// r.Handle("/static/", http.StripPrefix("/static", fileserver))
 
 	// start server
 	log.Println("App running on http://localhost:8080")
